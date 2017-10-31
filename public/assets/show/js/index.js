a745afaadd360dcefe346aadc7d33ff11b10b88a
wangsfox <wangsfox@foxmail.com>
2017-10-31 23:52:28 +0800

    生成图片

--- public/assets/show/js/index.js
+++ public/assets/show/js/index.js
@@ -78,3 +78,36 @@ $('#right_content').bind('swipeleft',function(){
         $('#head-left').attr('value','none');
     }
 });
+
+/*绘制canvas图像*/
+$(document).ready(function () {
+    var img = new Image();
+    img.src = "https://ws4.sinaimg.cn/large/006tNc79ly1fl1u1t7281j30ob0yiwsh.jpg";
+    img.onload=function () {
+       dwin(this);
+    }
+});
+
+function dwin(img) {
+    var canvas = document.getElementById('signCard');
+    var name = $('#name').val();
+    var room = $('#room').val();
+    var table = $('#table').val();
+    var imgput=$('#image-put');
+    var context = canvas.getContext('2d');  //获取对应的2D对象(画笔)
+    context.drawImage(img, 0, 0);
+    context.fillStyle = '#fff';
+    context.strokeStyle = '#fff'; //设置笔触的颜色
+    context.font = "150px '微软雅黑'"; //设置字体
+    context.textAlign='center';
+    context.textBaseline = 'hanging'; //在绘制文本时使用的当前文本基线
+    var x = Math.floor(canvas.width / 2); // change
+    var y = Math.floor(canvas.height / 2 - 200 / 2);
+    context.fillText(name, x, 490); //设置文本内容
+    context.font = "30px 'sans-serif'"; //设置字体
+    context.fillText(room, 320, 780);
+    context.fillText(table, 670, 780);
+    var src=canvas.toDataURL("image/png");
+    // console.log(src);
+    imgput.attr('src',src);
+}
